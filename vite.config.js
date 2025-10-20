56a142f5cce57f600cd8e38d0356773b94785561
Mohitverve <155957883+Mohitverve@users.noreply.github.com>
2025-10-20 21:08:07 +0530

    old vite

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    visualizer({ filename: 'bundle-visualizer.html', open: false }),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules/react')) return 'react-vendor';
-          if (id.includes('node_modules/antd')) return 'antd';
-          if (id.includes('@ant-design/icons')) return 'antd-icons';
-          // You can add more custom chunks here
-          if (id.includes('node_modules')) return 'vendor';
-        },
-      },
-    },
-    commonjsOptions: {
-      transformMixedEsModules: true,
-    },
-    chunkSizeWarningLimit: 800,
-  }
-});
+  plugins: [react()],
+})
